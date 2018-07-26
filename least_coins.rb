f96aa158ea9e658d42fbc7f29a57533e7dd905d4
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-26 17:37:30 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,12 +2,7 @@
 
 def least_coins(cents)
   coins =  {"Q" => 0, "D" => 0, "N" => 0, "P" => 0}
-if cents % 0.25 >= 1
-  >> "Q" 
-  coins["Q"] = 
-  puts ("Q") = cents / 0.25
-elsif cents -= "Q" * 0.25
-cents / 0.10 >= 1 
-puts ("D") = cents / 0.10
-end
+if cents >= 25 
+  "Q" = cents % 25 
+
 end
