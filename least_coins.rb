b3bebfe35c8b502a81fb24dcc986538270cb04b1
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-26 17:47:51 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,19 +4,19 @@
   coins =  {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
 if cents >= 25 
   coins[:quarters] = cents / 25 
-  cents -= 25 * [:quarters]
+  cents -= 25 * coins[:quarters]
 end
 if cents >= 10  
   coins[:dimes] = cents / 10  
-  cents -= 10 * [:dimes]
+  cents -= 10 * coins[:dimes]
 end
 if cents >= 5  
   coins[:nickels] = cents / 5  
-  cents -= 5 * [:nickels]
+  cents -= 5 * coins[:nickels]
 end
 if cents >= 1  
   coins[:pennies] = cents / 1  
-  cents -= 1 * [:pennies]
+  cents -= 1 * coins[:pennies]
 end
   end
 
